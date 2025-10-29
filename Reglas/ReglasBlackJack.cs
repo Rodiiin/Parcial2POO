fe171048d8eb5b789ea46bb58005e3b64c829bf0
Rodin <aragonleorodin@hotmail.com>
2025-10-28 18:08:52 -0600

    Creación de archivos Reglas y Turnos

---
+++ Reglas/ReglasBlackJack.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Parcial2POO.Reglas;
+
+public class ReglasBlackJack
+{
+
+}
---
+++ Turnos/TurnoUno.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Parcial2POO.Turnos;
+
+public class TurnoUno
+{
+
+}
