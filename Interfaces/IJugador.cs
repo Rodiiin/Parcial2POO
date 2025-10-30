b8fa5f89b5a3d661a3cbcb832ff24f7a88bfc1f5
Rodin <aragonleorodin@hotmail.com>
2025-10-30 10:35:41 -0600

    Se añade implementacion de interfaz y JuegoBlackJack

--- Interfaces/IJugador.cs
+++ Interfaces/IJugador.cs
@@ -4,5 +4,8 @@ namespace Parcial2POO.Cartas;
 
 public interface IJugador
 {
-
+    string Nombre { get; } 
+    void RecibirCarta(ICarta carta); //Para que el jugado pueda recibir carta
+    List<ICarta> ObtenerMano(); //Para que el jugaodr vea su mano.
+    bool DebePedirCarta(); //Decisión de pedir carta.
 }
